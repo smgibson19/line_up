87a20c68aadad01aa902bcee3751f83b07e0a872
smgibson19 <smjgibson17@gmail.com>
2020-04-27 18:25:19 -0400

    starting, added roster

--- main.cpp
+++ main.cpp
@@ -1,2 +1,45 @@
-//line up stuff
+//line up main
 
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include "player.h"
+
+int main(){
+
+	// creating the roster and adding players to roster
+	// in later development this will be read in by a file
+	std::vector<Player> roster;
+	Player player1("A", "OPP", 1);
+	roster.push_back(player1);
+	Player player2("B", "OUT", 2);
+	roster.push_back(player2);
+	Player player3("C", "MID", 3);
+	roster.push_back(player3);
+	Player player4("D", "OPP", 4);
+	roster.push_back(player4);
+	Player player5("E", "OUT", 5);
+	roster.push_back(player5);
+	Player player6("F", "MID", 6);
+	roster.push_back(player6);
+	Player player7("G", "OPP", 7);
+	roster.push_back(player7);
+	Player player8("H", "OUT", 8);
+	roster.push_back(player8);
+	Player player9("I", "MID", 9);
+	roster.push_back(player9);
+	Player player10("J", "OPP", 10);
+	roster.push_back(player10);
+	Player player11("K", "OUT", 11);
+	roster.push_back(player11);
+	Player player12("L", "MID", 12);
+	roster.push_back(player12);
+
+
+
+
+
+
+}

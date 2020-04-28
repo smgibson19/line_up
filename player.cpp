84a3d30a2a6f04993f91fffc090a2cbb844ad7e7
smgibson19 <smjgibson17@gmail.com>
2020-04-27 20:50:46 -0400

    comented out player wins/losses

--- player.cpp
+++ player.cpp
@@ -7,4 +7,58 @@
 #include "football_game.h"
 #include "football_team.h"
 
-//Constructors
+// Constructors
+Player::Player(){
+	wholeName="";
+	position="";
+	num=0;
+	wins=0;
+	losses=0;
+}
+
+Player::Player(std::string name, std::string pos, int number){
+	wholeName= name;
+	position= pos;
+	num= number;
+	wins=0;
+	losses=0;
+}
+
+// Getters
+std::string Player::getName(){
+	return wholeName;
+}
+
+std::string Player::getPosition(){
+	return position;
+}
+
+int Player::getNumber(){
+	return num;
+}
+
+// int Player::getWins(){
+// 	return wins;
+// }
+
+// int Player::getLosses(){
+// 	return losses;
+// }
+
+// // Setters
+// void Player::addWin(){
+// 	wins++;
+// }
+
+// void Player::addLoss(){
+// 	losses++;
+// }
+
+// // Methods
+// double Player::calcWinPercent(){
+// 	return wins/(wins+losses);
+// }
+
+// double Player::calcLossPercent(){
+// 	return losses/(wins+losses);
+// }

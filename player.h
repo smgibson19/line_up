0b65137b1761d94c1f5a3158f0891ce803e42994
smgibson19 <smjgibson17@gmail.com>
2020-05-02 19:13:57 -0400

    forgot getter for WOGP

--- player.h
+++ player.h
@@ -12,16 +12,19 @@ public:
 	std::string getPosition() const; 
 	int getNumber() const;
 	int getWins();
-	int gamesPlayed();
+	int numGamesPlayed();
+	int getWOGP();
 	
 	// Setters
 	void addWin();
 	void addGamePlayed();
+	void calcWOGP();
 
 // private variables
 private:
 	std::string wholeName, position;
 	int num, wins, gamesPlayed;
+	double winsOverGamesPlayed;
 };
 
 #endif
